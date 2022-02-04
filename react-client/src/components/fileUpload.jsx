2b10b34b8abfcb87bb2760b3802780f3e8b36c1b
riyabelle25 <riyaelizabethj@gmail.com>
2022-02-04 22:09:26 +0530

    minor fixes

--- react-client/src/components/fileUpload.jsx
+++ react-client/src/components/fileUpload.jsx
@@ -17,7 +17,7 @@ class Main extends React.Component {
     let data = new FormData();
     data.append('file', this.uploadInput.files[0]);
 
-    fetch('http://192.168.253.192:4444/api/upload', {
+    fetch('https://pod-matcher.herokuapp.com:5000/api/upload', {
         method: 'POST',
         // mode: 'no-cors',
         body: data,
